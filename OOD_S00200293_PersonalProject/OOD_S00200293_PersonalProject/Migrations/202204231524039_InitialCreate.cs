21a34890a5a37ca042c322d11bc1faf26bf2f6d7
StevenK418 <stevenkelly418@gmail.com>
2022-04-27 10:44:22 +0100

    => Added new Database migration to handle new structure of movie Objects.

---
+++ OOD_S00200293_PersonalProject/OOD_S00200293_PersonalProject/Migrations/202204231524039_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿namespace OOD_S00200293_PersonalProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Movies",
+                c => new
+                    {
+                        MovieID = c.String(nullable: false, maxLength: 128),
+                        Title = c.String(),
+                        Year = c.String(),
+                        ImbdRating = c.String(),
+                        Poster = c.String(),
+                        Plot = c.String(),
+                        Rated = c.String(),
+                        Director = c.String(),
+                    })
+                .PrimaryKey(t => t.MovieID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Movies");
+        }
+    }
+}
---
+++ OOD_S00200293_PersonalProject/OOD_S00200293_PersonalProject/Migrations/202204270940226_AlteredIMDBRating.cs
@@ -0,0 +1,20 @@
+﻿namespace OOD_S00200293_PersonalProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AlteredIMDBRating : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Movies", "imdbRating", c => c.String());
+            DropColumn("dbo.Movies", "ImbdRating");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Movies", "ImbdRating", c => c.String());
+            DropColumn("dbo.Movies", "imdbRating");
+        }
+    }
+}
